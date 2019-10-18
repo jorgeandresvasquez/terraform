03d3035038956d5c0c810cffb8a994357e13e5fb
jorgevasquez <jorge.vasquez@slalom.com>
2019-10-18 11:47:44 -0400

    Added script to bump versions

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 This repo contains generic terraform modules
 
-## Steps to change commit, push and tag new version:
+## Steps to commit, push and tag new version:
 1. Commit your local changes and push them to master
 2. Execute:
 ``` bash
--- repoctl/bump_version.py
+++ repoctl/bump_version.py
@@ -1,19 +1,40 @@
 import os
 import subprocess
+import yaml
+
+PROJECT_ROOT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../')
+GLOBALS_PATH = os.path.join(PROJECT_ROOT_PATH, 'global.yml')
 
 # Simple script to bump the patch number of the version (i.e., from 2.1.11 to 2.1.12)
 
-with open(os.path.join('../global.yml'), 'r') as f:
-    __version__ = None
-    exec(f.read())
+def read_from_globals(global_property):
+    external_vars = {}
+    if not os.path.isfile(GLOBALS_PATH):
+        raise RuntimeError(
+            "Could not find file specified in variable {}".format(GLOBALS_PATH))
 
-if __version__:
-    version = [int(x) for x in __version__.split('.')]
-    if len(version) == 3:
-        version[2] += 1
-    __version__ = '.'.join([str(x) for x in version])
-    with open(os.path.join('../global.yml'), 'w') as f:
-        f.write('__version__ = \'{}\''.format(__version__))
+    with open(GLOBALS_PATH, 'r') as fd:
+        external_vars = yaml.load(fd.read(), Loader=yaml.FullLoader)
+        return external_vars[global_property]
 
-subprocess.check_output(['git', 'commit', '../global.yml', '-m', 'bump version to {}'.format(__version__)])
-subprocess.check_output(['git', 'tag', __version__])
+def set_global_value(global_property_name, global_property_value):
+    with open(GLOBALS_PATH) as f:
+        doc = yaml.load(f, Loader=yaml.FullLoader)
+
+    doc[global_property_name] = global_property_value
+
+    with open(GLOBALS_PATH, 'w') as f:
+        yaml.dump(doc, f)
+
+current_version = read_from_globals('versionNumber')
+if current_version:
+    print(current_version)
+    version_arr = [int(x) for x in current_version.split('.')]
+    if len(version_arr) == 3:
+        version_arr[2] += 1
+    new_version = '.'.join([str(x) for x in version_arr])
+    set_global_value('versionNumber', new_version)
+    print(GLOBALS_PATH)
+    subprocess.check_output(['git', 'commit', GLOBALS_PATH, '-m', 'bumped version to {}'.format(new_version)])
+    subprocess.check_output(['git', 'tag', new_version])
+    subprocess.check_output(['git', 'push', '--follow-tags'])
